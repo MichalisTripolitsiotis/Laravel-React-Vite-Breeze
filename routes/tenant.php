16418e930b8c395b355f1776a2d77b159c4bcca1
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2023-07-11 11:36:19 +0300

    Require web routes in tenant

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,13 +3,8 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
-use App\Http\Controllers\PhoneVerificationController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
-use Illuminate\Foundation\Application;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,30 +23,5 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    });
-
-    Route::prefix('/verify')->group(function () {
-        Route::get('/phone', [PhoneVerificationController::class, 'index'])->name('verify.phone');
-        Route::post('/phone', [PhoneVerificationController::class, 'verify'])->name('verify.phone.verify');
-    });
-
-
-    Route::middleware(['auth', 'verified', 'phone_verify'])->group(function () {
-        Route::get('/dashboard', function () {
-            return Inertia::render('Dashboard');
-        })->name('dashboard');
-
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-        Route::resource('users', UserController::class);
-    });
+    require __DIR__ . '/web.php';
 });
